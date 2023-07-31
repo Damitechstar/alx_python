b587b7ad8bf03772c3a5ddb2d4cb88e5a9bd12d0
Damilola Onadeko <damiojimadu@gmail.com>
2023-07-31 21:45:27 +0100

    corrections

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,8 +2,11 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end="")
-    if i < 8 or j < 9:
-        print(", ", end="")
-    else:
-        print("")
+        print("{:02d}".format(i) + ", " + "{:02d}".format(j), end="")
+        if i < 8 or j < 9:
+            print(",", end=" ")
+        else:
+            print("")
+
+        
+
