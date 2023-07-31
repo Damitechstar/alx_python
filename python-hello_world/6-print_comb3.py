8dff99035bb2079cfa9f4b8b670b3140124efd2f
Damilola Onadeko <damiojimadu@gmail.com>
2023-07-31 21:32:48 +0100

    corrections

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
-for i in range(1, 10):
+for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i:0d}{j:0d}", end=", " if i != 8 or j != 9 else "\n")
+        print("{:d}{:d}".format(i, j), end="")
+    if i < 8 or j < 9:
+        print(", ", end="")
+    else:
+        print("")
