29689fdd8442d23c53b1402db39ef4b5b964f69f
Damilola Onadeko <damiojimadu@gmail.com>
2023-07-31 18:19:16 +0100

    New file created, corrections made to another file

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(10):
-    for j in range(i + 1, 10):
+    for j in range(i + 1, 10):  # Corrected line
         print("{:02d}, ".format(i) + "{:02d}".format(j), end="")
-print() 
+print()  # Print a new line at the end
