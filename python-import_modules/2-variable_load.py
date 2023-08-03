8b8a105d10bcf562781e2df5e0c4250f8cf46910
Damilola Onadeko <damiojimadu@gmail.com>
2023-08-04 00:15:17 +0100

    new file created

---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,10 @@
+
+if __name__ == "__main__":
+    main()
+    
+def main():
+# Import the variable a from the file
+    from variable_load_2 import a
+
+# Print the value of the variable a
+    print("The value of 'a' is:", a)
