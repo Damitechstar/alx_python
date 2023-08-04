e803d9c0adb3e375304e5b9b473c6da0ae8ad1f7
Damilola Onadeko <damiojimadu@gmail.com>
2023-08-04 21:35:13 +0100

    corrections

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import List
-
-def remove_c(my_string: str) -> str:
+def no_c(my_string):
     new_string = ""
     for char in my_string:
         if char.lower() != 'c':
             new_string += char
     return new_string
+    
+    
