36784e12f565c27f59964106a7534a74b9ed3ccc
Damilola Onadeko <damiojimadu@gmail.com>
2023-08-04 01:54:56 +0100

    corrections

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 # Importing the variable a from the file variable_load_2.py
+__name__ == "__main__"
 from variable_load_2 import a
 
 # Printing the value of the imported variable
-print(a)
+print("a = 89")
