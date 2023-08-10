dc33f5fe5e13d7decaa9ac3f18df9be5a8f0af02
Damilola Onadeko <damiojimadu@gmail.com>
2023-08-10 18:03:22 +0100

    corrections

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" This module defines the Square class and demonstrates its usage """
 
-class Square: """ The class square represents a square shape and provides methods for calculating its area and perimeter """
+# This module contains the Square class, which represents a square shape and provides methods for calculating its area and perimeter.
 
-def __init__(self, size):
-    self.__size = size
+class Square:
+    """This class represents a square shape and provides methods for calculating its area and perimeter."""
 
-def get_size(self):
-    return self.__size
+    def __init__(self, size):
+        self.__size = size
 
-def set_size(self, size):
-    self.__size = size
+    def get_size(self):
+        return self.__size
 
-def calculate_area(self):
-    return self.__size ** 2
+    def set_size(self, size):
+        self.__size = size
 
-def calculate_perimeter(self):
-    return 4 * self.__size
+    def calculate_area(self):
+        return self.__size ** 2
+
+    def calculate_perimeter(self):
+        return 4 * self.__size
