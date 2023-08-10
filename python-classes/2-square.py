e12a656b9a569d901b013df74dbc526c04b31ae6
Damilola Onadeko <damiojimadu@gmail.com>
2023-08-10 21:51:32 +0100

    new files created

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module defines the Square class, which represents a square shape."""
+
+class Square:
+    """Represents a square shape with a given size. The size must be a non-negative integer."""
+
+    def __init__(self, size=0):
+        """
+            Constructs a square object with the specified size.
+
+            Args:
+                size (int, optional): The size of the square. Defaults to 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+            Calculates and returns the area of the square.
+        """
+        return self.__size ** 2
