64f87a5a5bb3953b26d13a8e70e7ed5f832f5ac4
Damilola Onadeko <damiojimadu@gmail.com>
2023-08-10 21:37:05 +0100

    documentation added

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 
+""" This module contains the Square class, which defines a square."""
 class Square:
+    """A class representing a square."""
+
     def __init__(self, size=0):
+        """
+        Initializes a square object with a given size.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
