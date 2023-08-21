fa6ce162e9446d2716b6305b815121e77ecd6721
Damilola Onadeko <damiojimadu@gmail.com>
2023-08-22 00:51:01 +0100

    new files created

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Contains a function to write an empty class BaseGeometry
+"""
+class BaseGeometry:
+    """
+    Empty class representing the base geometry.
+    """
+    pass
