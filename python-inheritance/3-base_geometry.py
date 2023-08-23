be4a9789810e2be7a9bbddd639b6b95cfdc8bda0
Damilola Onadeko <damiojimadu@gmail.com>
2023-08-24 00:14:52 +0100

    more documentation added

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """A custom metaclass for the BaseGeometry class."""
 class BaseGeometryMetaClass(type):
+    """Class to remove __init__subclass attribute in base geometry class"""
+
     def __dir__(cls):
         attrs = super().__dir__()  # Get the default list of attributes
         attrs = [attr for attr in attrs if attr != "__init_subclass__"]  # Exclude "__init_subclass__"
@@ -8,6 +10,7 @@ class BaseGeometryMetaClass(type):
 
 
 class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    """Creates an empty class"""
     def __dir__(self):
         attrs = super().__dir__()  # Get the default list of attributes
         attrs = [attr for attr in attrs if attr != "__init_subclass__"]  # Exclude "__init_subclass__"
