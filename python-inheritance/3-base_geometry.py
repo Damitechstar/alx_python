dc52e7156dead2e1505f12589db0807833f6d663
Damilola Onadeko <damiojimadu@gmail.com>
2023-08-24 00:00:47 +0100

    corrections

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """
-    Empty class representing the base geometry.
-    """
-    pass
+class BaseGeometryMetaClass(type):
+    def __dir__(cls):
+        attrs = super().__dir__()  # Get the default list of attributes
+        attrs = [attr for attr in attrs if attr != "__init_subclass__"]  # Exclude "__init_subclass__"
+        return attrs
 
 
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    def __dir__(self):
+        attrs = super().__dir__()  # Get the default list of attributes
+        attrs = [attr for attr in attrs if attr != "__init_subclass__"]  # Exclude "__init_subclass__"
+        return attrs
