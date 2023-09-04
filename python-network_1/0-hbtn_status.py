282038e922fc863921063faffeaec571d57830d2
Damilola Onadeko <damiojimadu@gmail.com>
2023-09-04 12:53:19 +0100

    new file created

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -11,7 +11,7 @@ response = requests.get(url)
 
 if response.status_code == 200:
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
+    print("\t- type: {}".format(type(response.text))) 
     print("\t- content: {}".format(response.text))
 
 else:
---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL 
+and displays the value of the variable
+"""
+
+import requests
+import sys 
+
+if __name__ == "__main__":
+    # Get the URL from command line arguments
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        request_id = response.headers.get('X-Request-Id') 
+        
+        "Displays the value of X-Request-Id"
+        if request_id:
+            print(request_id)
+            
+        else:
+            print("Request failed with status code: {}".format(response.status_code)) 
+            
