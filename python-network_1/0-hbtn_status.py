0d6528fd2b0e754241b4c2d246e08b31fbb8032e
Damilola Onadeko <damiojimadu@gmail.com>
2023-09-04 12:10:09 +0100

    new file created

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that fetches URL
+"""
+
+import requests
+
+url = "https://alu-intranet.hbtn.io/status"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+
+else:
+    print("Error: {}".format(response.status_code))
