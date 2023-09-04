8b1f416ed8033c1191740eedfa678280b0daef2a
Damilola Onadeko <damiojimadu@gmail.com>
2023-09-04 13:04:34 +0100

    corrections

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL 
-and displays the value of the variable
 """
-
+    Script use the requests and sys packages to send a request  to a given URL 
+    and display the variable value Request-Id in the response header
+"""
 import requests
-import sys 
+import sys
 
 if __name__ == "__main__":
     # Get the URL from command line arguments
     url = sys.argv[1]
-
+    
+    # Send a GET request to the URL
     response = requests.get(url)
 
+    # Check if the request was successful
     if response.status_code == 200:
-        request_id = response.headers.get('X-Request-Id') 
-        
-        "Displays the value of X-Request-Id"
+
+        # Get the value of the 'X-Request-Id' h
+        request_id = response.headers.get('X-Request-Id')
+
+        # Display the value of 'X-Request-Id'
         if request_id:
             print(request_id)
-            
-        else:
-            print("Request failed with status code: {}".format(response.status_code))
+    else:
+        print("Request failed with status code: {}".format(response.status_code))
             
